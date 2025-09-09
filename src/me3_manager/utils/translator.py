27ce9336df7e20b9b01ffb7dbad877704e205c2b
Justin Koh <j@ustink.org>
2025-09-10 02:10:29 +0800

    Remove redundant super() args
    Since Python 3

--- src/me3_manager/utils/translator.py
+++ src/me3_manager/utils/translator.py
@@ -15,7 +15,7 @@ class Translator:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(Translator, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
